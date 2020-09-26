3da213a37ec4fcb4a9bcabb398ca7ee0cc5d8ee4
Ralph <tongolralph@yahoo.ca>
2020-09-26 12:00:02 -0400

    Code has been finalize

--- app/src/main/java/com/example/ralphtongol_comp304lab1_ex1/MainActivity.java
+++ app/src/main/java/com/example/ralphtongol_comp304lab1_ex1/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.ralphtongol_comp304lab1_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Life Cycle Methods Demonstration");
     }
 }
--- app/src/main/java/com/example/ralphtongol_comp304lab1_ex1/NewActivity.java
+++ app/src/main/java/com/example/ralphtongol_comp304lab1_ex1/NewActivity.java
@@ -2,19 +2,21 @@ package com.example.ralphtongol_comp304lab1_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class NewActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
 
-        TextView textView = (TextView) findViewById(R.id.AIActivity);
+        TextView textView = findViewById(R.id.AIActivity);
         textView.setText("\t\tAIActivity:\n" +
-                         "\t\t\t\tonCreate executed\n" +
-                         "\t\t\t\tonStart executed");
+                "\t\t\t\tonCreate executed\n" +
+                "\t\t\t\tonStart executed");
     }
 }
